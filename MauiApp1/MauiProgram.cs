0b86ecb35f5a839d996894bc786668ffcc12330d
choisy-k <99621344+choisy-k@users.noreply.github.com>
2023-10-11 20:08:26 +1300

    Upload the project

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,37 @@
+﻿using MauiApp1.ViewModel;
+using Microsoft.Extensions.Logging;
+
+namespace MauiApp1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            //these will be automatically registered with the dependency service 
+            // AddSingleton is a global that creates a copy of the oage and keep the memory, else transient will be created and destroyed the current copy
+
+            builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<MainViewModel>();
+
+            builder.Services.AddTransient<DetailPage>();
+            builder.Services.AddTransient<DetailViewModel>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
